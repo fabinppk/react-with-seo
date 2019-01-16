6f383817b9a319c3acae8afb76221fcc66bf1b23
Fabiano Correia <fabinppk@hotmail.com>
2019-01-16 15:03:40 -0200

    troca para usig class react component

--- pages/dev.js
+++ pages/dev.js
@@ -5,65 +5,67 @@ import "../scss/style.scss";
 import "../scss/dev.scss";
 import NextSeo from 'next-seo';
 
-const User = (props) => (
-  <div>
-    <NextSeo
-      config={{
-        title: props.user.nome + ' - Desenvolvedor Bonitour',
-        description: props.user.nome + ' - Lista de desenvolvedores que atuam na Bonitour Viagens e Turismo'
-      }}
-    />
-    <Navbar />
-    <div className="playground">
-      <div className="profile">
-        <Link href={"/"}>
-          <a>
-            <div className="back-button">
-              <div className="arrow-wrap">
-                <span className="arrow-part-1"></span>
-                <span className="arrow-part-2"></span>
-                <span className="arrow-part-3"></span>
+export default class User extends React.Component {
+
+  static async getInitialProps ({ query: { nome } }) {
+    const requisicao =  {
+                      url: 'https://api.bonitour.com.br' + '/api/v3/dev/' + nome,
+                      object: {
+                        method: 'GET',
+                        headers: {
+                          'Accept': 'application/json',
+                          'Content-Type': 'application/json',
+                          'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRpQGJvbml0b3VyLmNvbS5iciIsImV4cCI6MTU3OTA5MDg3M30.eyLkJgU6YKSuX5HQjuW3oCfl7GEpkKf_mGlQnVod7bY',
+                          'secret_key': 'vSMtK8VnNI97RGxrEFM2Dg'
+                        }
+                      }
+                    };
+
+    const response = await fetch(requisicao.url, requisicao.object);
+    const user = await response.json();
+    console.log(user);
+    return { user: user.dev }
+  }
+
+  onError(e) {
+    e.target.onerror = null;
+    e.target.src="https:bonitour.com.br/assets/images/sem_imagem.jpg";
+  }
+
+  render () {
+    return (
+      <div>
+        <NextSeo
+          config={{
+            title: this.props.user.nome + ' - Desenvolvedor Bonitour',
+            description: this.props.user.nome + ' - Lista de desenvolvedores que atuam na Bonitour Viagens e Turismo'
+          }}
+        />
+        <Navbar />
+        <div className="playground">
+          <div className="profile">
+            <Link href={"/"}>
+              <a>
+                <div className="back-button">
+                  <div className="arrow-wrap">
+                    <span className="arrow-part-1"></span>
+                    <span className="arrow-part-2"></span>
+                    <span className="arrow-part-3"></span>
+                  </div>
+                </div>
+              </a>
+            </Link>
+            <div className="p">
+              <div className="avatar">
+                <img onError={ this.onError } src={ "https:bonitour.com.br/assets/images/usuarios_fotos/" + this.props.user.arquivo_assinatura } alt={ this.props.user.nome } />
               </div>
+              <p className="name">{ this.props.user.nome }</p>
+              <p className="role">Email: { this.props.user.email }</p>
+              <p className="role">Cargo: { this.props.user.cargo_descricao }</p>
             </div>
-          </a>
-        </Link>
-        <div className="p">
-          <div className="avatar">
-            <img onError={ User.onError } src={ "https:bonitour.com.br/assets/images/usuarios_fotos/" + props.user.arquivo_assinatura } alt={ props.user.nome } />
           </div>
-          <p className="name">{ props.user.nome }</p>
-          <p className="role">Email: { props.user.email }</p>
-          <p className="role">Cargo: { props.user.cargo_descricao }</p>
         </div>
       </div>
-    </div>
-  </div>
-);
-
-User.onError = function(e) {
-  e.target.onerror = null;
-  e.target.src="https:bonitour.com.br/assets/images/sem_imagem.jpg";
-};
-
-User.getInitialProps = async function(props) {
-
-  const requisicao =  {
-                    url: 'https://api.bonitour.com.br' + '/api/v3/dev/' + props.query.nome,
-                    object: {
-                      method: 'GET',
-                      headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRpQGJvbml0b3VyLmNvbS5iciIsImV4cCI6MTU3OTA5MDg3M30.eyLkJgU6YKSuX5HQjuW3oCfl7GEpkKf_mGlQnVod7bY',
-                        'secret_key': 'vSMtK8VnNI97RGxrEFM2Dg'
-                      }
-                    }
-                  };
-
-  const response = await fetch(requisicao.url, requisicao.object);
-  const user = await response.json();
-  console.log(user);
-  return { user: user.dev }
-};
-
-export default User;
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -5,47 +5,50 @@ import Navbar from "../components/Navbar";
 import "../scss/style.scss";
 import NextSeo from 'next-seo';
 
-const Index = (props) => (
-  <section className="page-section">
-    <NextSeo
-      config={{
-        title: 'Desenvolvedores Bonitour',
-        description: 'Lista de desenvolvedores que atuam na Bonitour Viagens e Turismo'
-      }}
-    />
-    <Navbar />
-    <div className="block-page">
-      <h1>Lista de Desenvolvedores Bonitour</h1>
-      {props.users.devs.map(user => (
-        <Link key={user.id} as={`/dev/${user.nome}`.replace(/ /g, '-').normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()} href={`/devs?id=${user.nome}`.replace(/ /g, '-').normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()}>
-          <a className="usuarios">
-            <p className="nome">Nome: { user.nome }</p>
-          </a>
-        </Link>
-      ))}
-    </div>
-  </section>
-);
+export default class Index extends React.Component {
 
-Index.getInitialProps = async function(props) {
-  const requisicao =  {
-                    url: 'https://api.bonitour.com.br' + '/api/v3/devs',
-                    object: {
-                      method: 'GET',
-                      headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRpQGJvbml0b3VyLmNvbS5iciIsImV4cCI6MTU3OTA5MDg3M30.eyLkJgU6YKSuX5HQjuW3oCfl7GEpkKf_mGlQnVod7bY',
-                        'secret_key': process.env.REACT_APP_SECRET_KEY
+  static async getInitialProps () {
+
+    const requisicao =  {
+                      url: 'https://api.bonitour.com.br' + '/api/v3/devs',
+                      object: {
+                        method: 'GET',
+                        headers: {
+                          'Accept': 'application/json',
+                          'Content-Type': 'application/json',
+                          'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRpQGJvbml0b3VyLmNvbS5iciIsImV4cCI6MTU3OTA5MDg3M30.eyLkJgU6YKSuX5HQjuW3oCfl7GEpkKf_mGlQnVod7bY',
+                          'secret_key': 'vSMtK8VnNI97RGxrEFM2Dg'
+                        }
                       }
-                    }
-                  };
+                    };
 
-  const response = await fetch(requisicao.url, requisicao.object);
+    const response = await fetch(requisicao.url, requisicao.object);
+    const users = await response.json();
+    console.log(users);
+    return { users: users }
+  }
 
-  const users = await response.json();
-  console.log(users);
-  return { users: users }
+  render () {
+    return (
+      <section className="page-section">
+        <NextSeo
+          config={{
+            title: 'Desenvolvedores Bonitour',
+            description: 'Lista de desenvolvedores que atuam na Bonitour Viagens e Turismo'
+          }}
+        />
+        <Navbar />
+        <div className="block-page">
+          <h1>Lista de Desenvolvedores Bonitour</h1>
+          {this.props.users.devs.map(user => (
+            <Link key={user.id} as={`/dev/${user.nome}`.replace(/ /g, '-').normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()} href={`/devs?id=${user.nome}`.replace(/ /g, '-').normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()}>
+              <a className="usuarios">
+                <p className="nome">Nome: { user.nome }</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </section>
+    )
+  }
 }
-
-export default Index;
